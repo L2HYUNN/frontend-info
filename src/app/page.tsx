4386e2f0150fd7dc48826a3daa6064b3f7244c42
L2HYUNN <donghyuni98@gmail.com>
2024-02-29 19:30:55 +0900

    style: init vanilla-extract to style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { myStyle } from '@/styles/style.css';
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className={myStyle}>Home</h1>
       <Link href="/about">About</Link>
     </div>
   );
